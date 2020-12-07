80fe1886b6a3ae8e86975bb0c6700f1bacd72ec1
Mutiara Hardiani Mahe <39557008+mutiaramahee@users.noreply.github.com>
2020-12-07 10:23:47 +0700

    Perbarui layout, ini khusus buat dashboard aja

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -2,17 +2,68 @@
 <html>
 <head>
 @include('include.meta')
-  <title>Dashboard</title>
+  <title>Kokeru | Dashboard</title>
 </head>
 <body>
-  <!-- Sidenav -->
-  @include('include.sidenav')
   <!-- Main content -->
   <div class="main-content" id="panel">
     <!-- Topnav -->
-    @include('include.topnav')
+    <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Search form -->
+          <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+            <div class="form-group mb-0">
+              <div class="input-group input-group-alternative input-group-merge">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fas fa-search"></i></span>
+                </div>
+                <input class="form-control" placeholder="Search" type="text">
+              </div>
+            </div>
+            <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </form>
+          <!-- Navbar links -->
+          <ul class="navbar-nav align-items-center  ml-md-auto ">
+            <li class="nav-item d-xl-none">
+              <!-- Sidenav toggler -->
+              <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin" data-target="#sidenav-main">
+              </div>
+            </li>
+          </ul>
+          <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
+            <li class="nav-item">
+              <a href="{{route('auth.login')}}" class="btn btn-default">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     <!-- Header -->
-    @include('include.header')
+    <div class="header bg-primary pb-6">
+      <div class="container-fluid">
+        <div class="header-body">
+          <div class="row align-items-center py-4">
+            <div class="col-lg-6 col-7">
+              <h6 class="h2 text-white d-inline-block mb-0">Jumat, 4 Desember 2020</h6>
+            </div>
+            <div class="col-lg-6 col-5 text-right">
+              <a class="" title="Previous" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="" title="Next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- Page content -->
     <div class="container-fluid mt--6">
       @yield('content')
