f95ab76a17c667ef355f73f8c82a4387f3d5ab51
Mutiara Hardiani Mahe <39557008+mutiaramahee@users.noreply.github.com>
2020-12-08 05:08:02 +0700

    Benerin tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,117 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login</title>
+  @include('include.meta')
+  <title>Kokeru | Login</title>
 </head>
-<body>
-  @if(session('status'))
-<p>{{ session('status') }}</p>
-  @endif
-  <form action="{{route('auth.login')}}" method="POST">
-    @csrf
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email"><br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password"><br>
-    <input type="checkbox" id="remember" name="remember">
-    <label for="remember">Remember me</label><br>
-    <button type="submit">Masuk</button>
-  </form>
-  @error('email')
-      <p>Email dan pass salah</p>
-  @enderror
+<body class="bg-default">
+  <!-- Navbar -->
+  <nav id="navbar-main" class="navbar navbar-horizontal navbar-transparent navbar-main navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="">
+        <img src="../assets/img/brand/white.png">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse navbar-custom-collapse collapse" id="navbar-collapse">
+        <div class="navbar-collapse-header">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a href="dashboard.html">
+                <img src="../assets/img/brand/blue.png">
+              </a>
+            </div>
+            <div class="col-6 collapse-close">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr class="d-lg-none" />
+        <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+          <li class="nav-item d-none d-lg-block ml-lg-4">
+            <a href="{{route('pages.home')}}" class="btn btn-neutral btn-icon">
+              <span class="btn-inner--icon">
+                <i class="fas fa-newspaper mr-2"></i>
+              </span>
+              <span class="nav-link-inner--text">Dashboard</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Main content -->
+  <div class="main-content">
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-5">
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <!-- Page content -->
+    <div class="container mt--7 pb-2">
+      <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-7">
+          @if(session('status'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <span class="alert-text"><strong>Gagal login</strong> {{ session('status') }}</span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+          @endif
+          @error('email')
+            <p>Email dan pass salah</p>
+          @enderror
 
-  @error('password')
-    <p>Email dan pass salah</p>
-  @enderror
+          @error('password')
+            <p>Email dan pass salah</p>
+          @enderror
+          <div class="card bg-secondary border-0 mb-0">
+            <div class="card-body px-lg-5 py-lg-5">
+              <div class="text-center text-muted mb-4">
+                <small>Sign in with credentials</small>
+              </div>
+              <form role="form" action="{{route('auth.login')}}" method="POST">
+                @csrf
+                <div class="form-group mb-3">
+                  <div class="input-group input-group-merge input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Email" type="email" name="email">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group input-group-merge input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                    </div>
+                    <input class="form-control" placeholder="Password" type="password" name="password">
+                  </div>
+                </div>
+                <div class="text-center">
+                  <input type="submit" name="submit" class="btn btn-primary my-4" value="Sign In"></input>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="row mt-2">
+            <div class="col-6">
+              <a href="#" class="text-light"><small>Forgot password?</small></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@include('include.script')
 </body>
 </html>
