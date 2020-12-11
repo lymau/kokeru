ae0bfeeb4d6e058ae7b2de933aec444afdaf5d6d
Mutiara Hardiani Mahe <39557008+mutiaramahee@users.noreply.github.com>
2020-12-11 16:57:01 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\RuangController;
+use App\Http\Controllers\CSController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\LaporanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,14 +30,21 @@ Route::get('/logout', [LogoutController::class, 'store'])->name('auth.logout');
 Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
 Route::post('/login', [LoginController::class, 'store']);
 
-// halaman manajer
+// manajer
 Route::get('/manajer', function(){
     return view('manajer.dashboard');
 })->name('manajer.dashboard');
 Route::get('/manajer/ruang', [RuangController::class, 'index'])->name('manajer.ruang.index');
-Route::resource('ruang', RuangController::class);
+Route::get('/manajer/cs', [CSController::class, 'index'])->name('manajer.cs.index');
+Route::get('/manajer/jadwal', [JadwalController::class, 'index'])->name('manajer.jadwal.index');
+Route::post('/manajer/jadwal', [JadwalController::class, 'index'])->name('manajer.jadwal.index');
 
-// dashboard cs
+Route::resource('ruang', RuangController::class);
+Route::resource('cs', CSController::class);
+Route::resource('jadwal', JadwalController::class);
+Route::resource('laporan', LaporanController::class);
+
+// cs
 Route::get('/cs', function(){
     return view('cs.dashboard');
 })->name('cs.dashboard');
