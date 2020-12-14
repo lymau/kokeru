2acc532d6f9e62a810c918a75038b2cd24e01079
Mutiara Hardiani Mahe <39557008+mutiaramahee@users.noreply.github.com>
2020-12-14 09:12:47 +0700

    tambah chart

--- resources/views/manajer/dashboard.blade.php
+++ resources/views/manajer/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Page content -->
-    <div class="container-fluid mt--6" style="height: 400px">
+    <div class="container-fluid mt--6">
       <div class="row">
         <div class="row">
             <div class="col-xl-3 col-md-6">
@@ -77,6 +77,71 @@
                 </div>
               </div>
             </div>
+          <div class="col-xl-12">
+          <div class="card">
+            <div class="card-header bg-transparent">
+              <div class="row align-items-center">
+                <div class="col">
+                  <h6 class="text-uppercase text-muted ls-1 mb-1">Performa</h6>
+                  <h5 class="h3 mb-0">Jumlah ruang bersih</h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+            <?php
+                $i=0;
+                foreach ($chart as $c) {
+                  $label[$i] = $c->label;
+                  $frec[$i] = $c->frec;
+                  $i++;
+                }
+            ?>
+            <canvas id="myChart" width="400" height="100"></canvas>
+              <script>
+              var ctx = document.getElementById('myChart').getContext('2d');
+              var myChart = new Chart(ctx, {
+                  type: 'bar',
+                  data: {
+                      labels: <?=json_encode($label);?>,
+                      datasets: [{
+                          label: '',
+                          data: <?=json_encode($frec);?>,
+                          backgroundColor: [
+                              'rgba(255, 99, 132, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(255, 206, 86, 0.2)',
+                              'rgba(75, 192, 192, 0.2)',
+                              'rgba(153, 102, 255, 0.2)',
+                              'rgba(255, 159, 64, 0.2)',
+                              'rgba(255, 99, 132, 0.2)'
+                          ],
+                          borderColor: [
+                              'rgba(255, 99, 132, 1)',
+                              'rgba(54, 162, 235, 1)',
+                              'rgba(255, 206, 86, 1)',
+                              'rgba(75, 192, 192, 1)',
+                              'rgba(153, 102, 255, 1)',
+                              'rgba(255, 159, 64, 1)',
+                              'rgba(255, 99, 132, 1)'
+                          ],
+                          borderWidth: 1
+                      }]
+                  },
+                  options: {
+                      scales: {
+                          yAxes: [{
+                              ticks: {
+                                  beginAtZero: true
+                              }
+                          }]
+                      }
+                  }
+              });
+              </script>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
 @endsection
