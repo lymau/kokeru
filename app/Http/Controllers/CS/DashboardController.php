4b7352119272067d7b428f8e80bd89cb2286571c
whitefay <nursabilly.kun@gmail.com>
2020-12-15 20:22:30 +0700

    hapus fungsi store di dashboardcontroller

--- app/Http/Controllers/CS/DashboardController.php
+++ app/Http/Controllers/CS/DashboardController.php
@@ -25,32 +25,4 @@ class DashboardController extends Controller
             'jadwal' => $jadwal
         ]);
     }
-
-    public function store(Request $request){
-        // validasi dulu
-        $this->validate($request, [
-            'foto' => 'required',
-            'foto.*' => ['required', 'mimetypes:image/*', 'max:2048'],
-            'video' => ['mimetypes:video/*', 'max:2048']
-        ]);
-        dd($request);
-
-        if ($request->hasFile('foto')){
-            foreach($request->file('foto') as $key => $file){
-                $path = $file->store('/assets/uploads', 'public');
-                $name = $file->getClientOriginalName();
-
-                $insert[$key]['name'] = $name;
-                $insert[$key]['path'] = $path;
-            }
-        }
-        
-        // $laporan_model = new Laporan;
-        // $laporan_model->store()
-        // $bukti_model = new Bukti;
-        // $bukti_model->nama_file = json_encode($insert);
-        // $bukti_model->save();
-        // return back()->with('success', 'Sudah berhasil diupload');
-
-    }
 }
