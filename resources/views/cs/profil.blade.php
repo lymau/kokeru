0c74de8717f0c9b234b337357aa34574d47ca70d
raarchell <racheliaptradr@gmail.com>
2020-12-16 15:47:17 +0700

    profil cs msh blm bisa

--- resources/views/cs/profil.blade.php
+++ resources/views/cs/profil.blade.php
@@ -21,7 +21,6 @@
               </button>
             </div>
           @endif
-
           @if ($errors->any())
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <strong>Whoops!</strong> Terjadi kesalahan<br><br>
@@ -32,35 +31,41 @@
                 </ul>
             </div>
           @endif
+
           <div class="card pb-5">
             <!-- Card header -->
             <div class="card-header">
               <div class="row align-items-center">
                 <div class="col-8">
-                  <h3 class="mb-0">Profil CS</h3>
+                  <h3 class="mb-0">Edit Profil</h3>
                 </div>
               </div>
             </div>
             <div class="card-body">
-              <form action="{{route('cs.profil', $profile->nama_user)}}" method="POST">
+            <form action="{{ route('cs.profil') }}" method="POST">
                 @csrf
-                @method('PUT')
+                @method('patch')
+              <input type="hidden" name="id" value="{{ $id }}">
                 <div class="form-group">
-                    <label for="example-text-input" class="form-control-label">ID</label>
-                    <input class="form-control" type="text" value="{{$profile->id}}" name="id" readonly>
-                </div>
-                <div class="form-group">
-                    <label for="example-search-input" class="form-control-label">Nama</label>
-                    <input class="form-control" type="text" value="{{$profile->nama_user}}" name="nama_user" required minlength="5" maxlength="255">
-                </div>
-                <div class="form-group">
-                    <label for="example-search-input" class="form-control-label">Email</label>
-                    <input class="form-control" type="email" value="{{$profile->email}}" name="email" required minlength="15" maxlength="50">
-                </div>
-                <div class="form-group">
-                    <input type="submit" name="submit" value="Update" class="btn btn-primary">
-                    <a href="{{ route('cs.profil') }}" class="btn btn-secondary">Cancel</a>
-                </div>
+                  <label for="nama" class="form-control-label">Nama</label>
+                <input class="form-control" type="text" value="{{ $nama }}" name="nama" id="nama" required>
+              </div>
+              <div class="form-group">
+                <label for="email" class="form-control-label">Email</label>
+              <input class="form-control" type="email" value="{{ $email}}" name="email" id="email" required>
+            </div>
+            <div class="form-group">
+              <label for="password" class="form-control-label">Password <small class="text-muted"></small></label>
+              <input class="form-control" type="password" name="password" id="password" minlength="8" required>
+          </div>
+          <div class="form-group">
+            <label for="password_confirmation" class="form-control-label">Re-type Password</label>
+            <input class="form-control" type="password" name="password_confirmation" id="password_confirmation" required>
+        </div>
+        <div class="form-group">
+          <input type="submit" name="submit" value="Update" class="btn btn-primary">
+          <a href="{{ route('cs.profil') }}" class="btn btn-secondary">Cancel</a>
+      </div>
               </form>
             </div>
           </div>
