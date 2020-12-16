e26e21b78809eb13507edc621cf18e081467dcee
Mutiara Hardiani Mahe <39557008+mutiaramahee@users.noreply.github.com>
2020-12-16 21:10:08 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,9 +10,13 @@ use Illuminate\Support\Facades\Hash;
 class ProfileController extends Controller
 {
     public function index(){
-        $manajer = Auth::user();
-
-        return view('manajer.profil', ['manajer' => $manajer]);
+        if(auth()->user()->manajer){
+            $manajer = Auth::user();
+            return view('manajer.profil', ['profil' => $manajer]);
+        } else{
+            $cs = Auth::user();
+            return view('manajer.profil', ['profil' => $cs]);
+        }
     }
 
     public function store(Request $request){
@@ -20,28 +24,53 @@ class ProfileController extends Controller
         $request->validate([
             'nama' => 'required|max:255',
             'email' => 'required|email|max:255',
-            'password' => 'required|confirmed'
         ]);
 
         $id = auth()->user()->id;
 
-        $pass = Hash::check($request->password, auth()->user()->password);
-
-        if ($pass){
-            $store = User::where('id', $id)
-            ->update([
-                'nama_user' => $request->nama,
-                'email' => $request->email,
-                'password' => bcrypt($request->password)
-            ]);
-            if ($store){
-                return redirect()->route('manajer.profil')->with('success', 'Data Anda berhasil diperbarui');
-            } else {
-                return redirect()->route('manajer.profil')->with('failed', 'Data Anda gagal diperbarui');
+        if(isset($request->password)){
+            if(!isset($request->password_new)){
+                if(auth()->user()->manajer){
+                    return redirect()->route('manajer.profil')->with('failed', 'Anda belum memasukkan password baru');
+                } else{
+                    return redirect()->route('akun')->with('failed', 'Anda belum memasukkan password baru');
+                }
             }
+            $pass = Hash::check($request->password, auth()->user()->password);
+            if ($pass){
+                $store = User::where('id', $id)
+                ->update([
+                    'nama_user' => $request->nama,
+                    'email' => $request->email,
+                    'password' => bcrypt($request->password_new)
+                ]);
+            } else {
+                if(auth()->user()->manajer){
+                    return redirect()->route('manajer.profil')->with('failed', 'Password tidak sama!');
+                } else{
+                    return redirect()->route('akun')->with('failed', 'Password tidak sama!');
+                }
+            }
+        } else{
+            $store = User::where('id', $id)
+                ->update([
+                    'nama_user' => $request->nama,
+                    'email' => $request->email,
+                ]);
         }
-        else {
-            return redirect()->route('manajer.profil')->with('failed', 'Password tidak sama!');
+
+        if ($store){
+            if(auth()->user()->manajer){
+                return redirect()->route('manajer.profil')->with('success', 'Data Anda berhasil diperbarui');
+            } else{
+                return redirect()->route('akun')->with('success', 'Data Anda berhasil diperbarui');
+            }
+        } else {
+            if(auth()->user()->manajer){
+                return redirect()->route('manajer.profil')->with('failed', 'Data Anda gagal diperbarui');
+            } else{
+                return redirect()->route('akun')->with('failed', 'Data Anda gagal diperbarui');
+            }
         }
     }
 }
